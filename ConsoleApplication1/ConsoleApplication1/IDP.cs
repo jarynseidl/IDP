deb76deae61cc309a53bfd700027b0a11c258938
jarynseidl <jarynseidl@gmail.com>
2017-01-31 15:25:38 -0700

    IDP Class

---
+++ ConsoleApplication1/ConsoleApplication1/IDP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class IDP
+    {
+        int IDP_SK;
+        int RtgSet_SK;
+        int startMoment;
+        int stopMoment;
+        String code;
+
+        public IDP(int IDP, int rtgSet, int startMoment, int stopMoment, String code)
+        {
+            this.IDP_SK = IDP;
+            this.RtgSet_SK = rtgSet;
+            this.startMoment = startMoment;
+            this.stopMoment = stopMoment;
+        }
+    }
+    
+}
